84a9c622055e725a543f6073cb239e63c13348f0
jwlee <dannis1124@naver.com>
2020-06-29 21:38:27 +0900

    Updates

---
+++ precache-manifest.ddf6eb51029202fb5f03d20248b2f953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27090cc982dd82ee2b0625b9854e2274",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "28749da282cc76822b66",
+    "url": "/movie/static/css/main.839fb3ee.chunk.css"
+  },
+  {
+    "revision": "162f0310b1e7fb3ead82",
+    "url": "/movie/static/js/2.8d282ef2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie/static/js/2.8d282ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28749da282cc76822b66",
+    "url": "/movie/static/js/main.ea4d2fbc.chunk.js"
+  },
+  {
+    "revision": "e031e007620c08d758dc",
+    "url": "/movie/static/js/runtime-main.ffb8bbce.js"
+  }
+]);
